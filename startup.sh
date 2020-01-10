82466f289c3cfaecdff549153c351e7235d78bb5
LannyYao <13896564373@139.com>
2020-01-10 15:01:29 +0800

    update

--- startup.sh
+++ startup.sh
@@ -1,20 +1,18 @@
 #! /bin/sh
 PORT=8080
 JAR_NAME=$1
-pid=`netstat -apn | grep $PORT | awk '{print $7}' | cut -d/ -f 1`
+pid=$(netstat -apn | grep $PORT | awk '{print $7}' | cut -d/ -f 1)
 
 if [ -n "$pid" ];then
  echo "$PID号进程尝试终止"
  kill -9 $pid
  echo "$PID端口进程终止成功"
- cd target
  echo "开始启动$PORT端口"
  nohup java -jar $JAR_NAME &
 fi
 
 
 if [ ! -n "$pid" ];then
- cd target
  echo "开始启动$PORT端口"
  nohup java -jar $JAR_NAME &
 fi
