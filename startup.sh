36690ea28bd5e2c5047828a6f1244eafd2e1d7d7
LannyYao <13896564373@139.com>
2020-01-10 15:22:45 +0800

    m

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@ PORT=8080
 JAR_NAME=$1
 pid=$(netstat -apn | grep $PORT | awk '{print $7}' | cut -d/ -f 1)
 if [ -n "$pid" ];then
- echo "$PID号进程尝试终止"
+ echo "$pid号进程尝试终止"
  kill -9 $pid
  echo "$PID端口进程终止成功"
  echo "开始启动$PORT端口"
